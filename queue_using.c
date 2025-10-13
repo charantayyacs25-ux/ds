b6be97bd2812cff1bcf09c5242337180593b30b9
charantayyacs25-ux <charantayya.cs25@bmsce.ac.in>
2025-10-13 09:49:46 +0530

    Add files via upload

---
+++ queue_using.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#define MAX 50
+
+int queue_array[MAX];
+int rear = -1;
+int front = -1;
+
+
+void insert() {
+    int add_item;
+    if (rear == MAX - 1)
+        printf("Queue Overflow\n");
+    else {
+        if (front == -1)
+            front = 0;
+        printf("Insert the element in queue: ");
+        scanf("%d", &add_item);
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+    }
+}
+
+
+void delete() {
+    if (front == -1 || front > rear) {
+        printf("Queue Underflow\n");
+        return;
+    } else {
+        printf("Element deleted from queue is: %d\n", queue_array[front]);
+        front = front + 1;
+    }
+}
+
+
+void display() {
+    int i;
+    if (front == -1)
+        printf("Queue is empty\n");
+    else {
+        printf("Queue is:\n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+    }
+}
+
+
+int main() {
+    int choice;
+    while (1) {
+        printf("\n1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
